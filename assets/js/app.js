700d3b78ba8d48b7a39fbf2215b5b031cdfc5973
amira <aialm4@student.monash.edu>
2017-10-23 01:18:35 +0300

    //
    //

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,116 @@
-var ViewModel=function(){
-var self=this;
-	
-self.locations=ko.observableArray();
-		
-	for(var i=0;i<model.length;i++){
-		
-		self.locations.push(new Location(model[i]));
-	}
+var model = [{
+    title: 'My Home',
+    location: {
+      lat: -37.825242,
+      lng: 144.965672
+    }
+  },
+  {
+    title: 'Crown ',
+    location: {
+      lat: -37.823514,
+      lng: 144.958076
+    }
+  },
+  {
+    title: 'federation Square ',
+    location: {
+      lat: -37.817972,
+      lng: 144.969041
+    }
+  },
+  {
+    title: 'City Square',
+    location: {
+      lat: -37.815871,
+      lng: 144.966901
+    }
+  },
+  {
+    title: 'Starbucks',
+    location: {
+      lat: -37.813480,
+      lng: 144.966305
+    }
+  },
+  {
+    title: 'Monash College',
+    location: {
+      lat: -37.813107,
+      lng: 144.966281
+    }
+  },
+  {
+    title: 'Albert Park',
+    location: {
+      lat: -37.845071,
+      lng: 144.968891
+    }
+  },
+  {
+    title: 'Port melbourne Beach',
+    location: {
+      lat: -37.842733,
+      lng: 144.935074
+    }
+  },
+  {
+    title: 'Williamstown',
+    location: {
+      lat: -37.861031,
+      lng: 144.885635
+    }
+  },
+  {
+    title: 'Monash University',
+    location: {
+      lat: -37.876954,
+      lng: 145.04425
+    }
+  },
+  {
+    title: 'Chadstone',
+    location: {
+      lat: -37.886235,
+      lng: 145.08296
+    }
+  },
+];
+
+var Location = function(data) {
+
+  var self = this;
+  self.title = data.title;
+  self.position = data.location;
 
 };
-		
-var model=[
-          {title: 'My Home', location: {lat: -37.825242, lng: 144.965672}},
-          {title: 'Crown ', location: {lat: -37.823514, lng: 144.958076}},
-          {title: 'federation ', location: {lat: -37.817972, lng: 144.969041}},
-          {title: 'City Square' , location: {lat: -37.815871, lng: 144.966901}},
-          {title: 'Starbucks', location: {lat: -37.813480, lng: 144.966305}},
-          {title: 'Monash College', location: {lat: -37.813107, lng: 144.966281}},
-		  {title: 'Albert Park', location: {lat: -37.845071, lng: 144.968891}},
-		  {title: 'Port melbourne Beach', location: {lat: -37.842733, lng: 144.935074}},
-		  {title: 'Williamstown', location: {lat: -37.861031, lng: 144.885635}},
-		  {title: 'Monash University', location: {lat: -37.876954, lng: 145.04425}},
-		  {title: 'Chadstone', location: {lat: -37.886235, lng: 145.08296}},
-        ];
-		
-		
-var Location=function(data){
+
+var ViewModel = function() {
+  var self = this;
+
+  self.myLocations = ko.observableArray();
+
+  self.userInput = ko.observable('');
+
+  for (var i = 0; i < model.length; i++) {
+
+    self.myLocations.push(new Location(model[i]));
+  }
+
+  self.doSomething = ko.computed(function(){
+    console.log(self.userInput());
+	  
+	  
+	  
+	  
+	  
+  });
+
 	
-	var self=this;
-	self.title=data.title;
-	self.position=data.location;
-		
+	
 };
 
- 
+
+
+
 ko.applyBindings(new ViewModel())
